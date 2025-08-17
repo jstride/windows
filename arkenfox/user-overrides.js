56f694d05bb1c2a64a8308d9a5ec2bedf68d03fb
Jake Stride <jake@rahd.ai>
2025-08-17 12:37:05 +0800

    Setting defaults for Arkenfox

---
+++ arkenfox/user-overrides.js
@@ -0,0 +1,10 @@
+user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
+user_pref("browser.shell.checkDefaultBrowser", false); // Check default browser
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+user_pref("keyword.enabled", true);
+user_pref("browser.urlbar.suggest.engines", true);
+user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+]
+user_pref("signon.rememberSignons", false); // Don't Save Passwords
+user_pref("extensions.formautofill.addresses.enabled", false); // [FF55+]
+user_pref("extensions.formautofill.creditCards.enabled", false); // [FF56+]
