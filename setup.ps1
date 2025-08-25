d1347aeeb383c43b63353fbd4f2d6279c1c2cf8f
Jake Stride <jake@stride.family>
2025-08-25 16:08:32 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -9,8 +9,7 @@ winget install 9NKSQGP7F2NH XP8JNQFBQH6PVF 9NZVDKPMR9RD XP9KHM4BK9FZ7Q 9PD9BHGLF
 9WZDNCRFJ4Q7 # LinkedIn
 
 # Set up Windows settings
-
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name MMTaskbarMode -Value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name MMTaskbarMode -Value 2
 Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sShortTime" -Value "HH:mm"
 Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sTimeFormat" -Value "HH:mm:ss"
 Stop-Process -Name explorer -Force
